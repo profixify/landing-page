f76f9e50d50a8e15896ccbcc6ab8e4b810f9808c
Ali Emre Şafak <aliemresafak@gmail.com>
2024-05-30 22:03:10 +0300

    feat: rename app name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="h-[calc(100vh-5rem)] flex items-center justify-center">
       <div className="flex flex-col">
         <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
-          Serman
+          Profixify
         </h1>
         <div className="flex flex-col text-xl max-w-2xl m-auto text-white">
           <span className="max-w-2xl m-auto mt-5 sm:text-center sm:text-2xl flex items-center gap-2">
